637a6e0394dba7297082779971e427c2fbc9366f
Lucas Mateus Silva <42013706+lucasmss@users.noreply.github.com>
2020-03-02 17:19:06 -0300

    Mudanças do JS
    Mudanças de JS

---
+++ arquivo.js
@@ -0,0 +1,14 @@
+validar = function (user){
+
+var usuario = document.getElementById("user");
+
+if(usuario == "admin"){
+
+    alert("Chamou a função");
+
+}else {
+
+    alert("não chamou a função");
+}  
+
+}
