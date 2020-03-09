9ab2220210b3c07f1743fbfd6c721d9c1b8d5129
Lucas Mateus Silva <42013706+lucasmss@users.noreply.github.com>
2020-03-09 18:33:30 -0300

    Update e adicionando a função no button salvar
    Update das funcionalidade e validação dos campos obrigatórios

--- js/arquivo.js
+++ js/arquivo.js
@@ -1,6 +1,6 @@
 function validarTelas(){
-    document.getElementById('div-login').style.display = 'none';
-    document.getElementById('div-cadastro').style.display = 'block';
+    document.getElementById('div-login').style.display = 'block';
+    document.getElementById('div-cadastro').style.display = 'none';
 
 }
 
@@ -23,5 +23,26 @@ function validarLogin(){
         alert("usuario ou senha incorreta");
         
         }  
+
+}
+
+function validarDados(){
+
+    var nome = document.getElementById('nome').value;
+    var quantidade = document.getElementById('quantidade').value;
+    var validade = document.getElementById('validade').value;
+    var tipoProduto = document.getElementById('tipoProduto').value;
+    var valor = document.getElementById('valor').value;
+  
+
+    if(nome != "" && quantidade != "" && validade != "" && tipoProduto != "" && valor !=""){
+
+        alert('mec');
+
+    } else{
+        
+        alert('Preencha os campos obrigatorios');
+  
+    }
 
 }
