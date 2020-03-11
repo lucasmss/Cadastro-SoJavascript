681f2fc4641e0290041456dced12af7f846c82ae
Lucas Mateus Silva <42013706+lucasmss@users.noreply.github.com>
2020-03-11 17:57:08 -0300

    Update de tabela, aplicando função
    Update de tabela aplicando a função

--- js/arquivo.js
+++ js/arquivo.js
@@ -1,6 +1,6 @@
 function validarTelas(){
-    document.getElementById('div-login').style.display = 'block';
-    document.getElementById('div-cadastro').style.display = 'none';
+    document.getElementById('div-login').style.display = 'none';
+    document.getElementById('div-cadastro').style.display = 'block';
 
 }
 
@@ -26,6 +26,8 @@ function validarLogin(){
 
 }
 
+
+
 function validarDados(){
 
     var nome = document.getElementById('nome').value;
@@ -33,16 +35,51 @@ function validarDados(){
     var validade = document.getElementById('validade').value;
     var tipoProduto = document.getElementById('tipoProduto').value;
     var valor = document.getElementById('valor').value;
-  
+
 
     if(nome != "" && quantidade != "" && validade != "" && tipoProduto != "" && valor !=""){
 
-        alert('mec');
+        alert("funciona!")
 
     } else{
         
-        alert('Preencha os campos obrigatorios');
+        alert('Preencha os campos obrigatórios');
   
     }
 
+}
+
+function adicionarDados(){
+
+    
+    var tabela = document.getElementById('tabela');
+    var qtnLinhas = tabela.rows.length;
+    var linha = tabela.insertRow(qtnLinhas);
+  
+    
+    var cellNome = linha.insertCell(0);
+    var cellQuantidade = linha.insertCell(1);
+    var cellValidade = linha.insertCell(2);
+    var cellTipoProduto = linha.insertCell(3);
+    var cellValor = linha.insertCell(4);
+
+    var nome = document.getElementById('nome').value;
+    var quantidade = document.getElementById('quantidade').value;
+    var validade = document.getElementById('validade').value;
+    var tipoProduto = document.getElementById('tipoProduto').value;
+    var valor = document.getElementById('valor').value;
+
+
+    cellNome.innerHTML = nome;
+    cellQuantidade.innerHTML = quantidade;
+    cellValidade.innerHTML = validade;
+    cellTipoProduto.innerHTML = tipoProduto;
+    cellValor.innerHTML = valor;
+
+}
+
+function removeDados(id){
+
+    teste = document.getElementById(id);
+    teste.parentNode.parentNode.removeChild(teste.parentNode);
 }
