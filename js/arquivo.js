910e09c774415f81d10ebbb98d755d8ed76eaefc
lucasmss16 <lucasmss16@gmail.com>
2020-03-27 05:27:04 -0300

    edição, função adicionada parcialmente

--- js/arquivo.js
+++ js/arquivo.js
@@ -26,6 +26,8 @@ function validarLogin(){
 
 }
 
+var contador = 0;
+
 function validarDados(){
 
     var nome = document.getElementById('nome').value;
@@ -34,6 +36,12 @@ function validarDados(){
     var tipoProduto = document.getElementById('tipoProduto').value;
     var valor = document.getElementById('valor').value;
     var emEstoque = document.getElementsByName('emEstoque').value;
+    if (document.getElementById('simRadio').checked) {
+        emEstoque  = document.getElementById('simRadio').value;
+      }
+      else{
+        emEstoque = document.getElementById('naoRadio').value;
+      }
     var descricao = document.getElementById('textArea').value;
 
 
@@ -52,101 +60,125 @@ function validarDados(){
     }
 
 }
-var contador = 0;
+
+
+
+function edit_row(no)
+{
+ document.getElementById("edit_button"+no).style.display="none";
+ document.getElementById("save_button"+no).style.display="block";
+	
+ var nome=document.getElementById('nome'+no);
+ var quantidade=document.getElementById('quantidade'+no);
+ var validade=document.getElementById('validade'+no);
+ //var tipoProduto = document.getSelection('tipoProduto'+no);
+ var valor = document.getElementById('valor'+no);
+ var emEstoque = document.getElementsByName('emEstoque');
+ if (document.getElementById('simRadio').checked) {
+    emEstoque = document.getElementById('simRadio').value;
+  }
+  else{
+    emEstoque = document.getElementById('naoRadio').value;
+  }
+ //var descricao = document.getElementById('textArea'+no);
+	
+ var nome_data=nome.innerHTML;
+ var quantidade_data=quantidade.innerHTML;
+ var validade_data=validade.innerHTML;
+ //var tipoPro_data=tipoProduto.innerHTML;
+ var valor_data=valor.innerHTML;
+ var emEstoque_data=emEstoque.innerHTML;
+ //var descricao_data=descricao.innerHTML;
+
+	
+ nome.innerHTML="<input type='text' id='nome_dado"+no+"' value='"+nome_data+"'>";
+ quantidade.innerHTML="<input type='number' id='quantidade_dado"+no+"' value='"+quantidade_data+"'>";
+ validade.innerHTML="<input type='date' id='validade_dado"+no+"' value='"+validade_data+"'>";
+ //tipoProduto.innerHTML="<input type='text' id='tipoProduto_dado"+no+"' value='"+tipoPro_data+"'>";
+ valor.innerHTML="<input type='number' id='valor_dado"+no+"' value='"+valor_data+"'>";
+ emEstoque.innerHTML="<input type='text' id='emEstoque_dado"+no+"' value='"+emEstoque_data+"'>";
+ //descricao.innerHTML="<input type='text' id='descricao_dado"+no+"' value='"+descricao_data+"'>";
+ 
+}
+
+
+function save_row(no)
+{
+    var name_val=document.getElementById("nome_dado"+no).value;
+    var quantidade_val=document.getElementById("quantidade_dado"+no).value;
+    var validade_val=document.getElementById("validade_dado"+no).value;
+   // var tipoProduto_val=document.getElementById("tipoProduto_dado"+no).value;
+    var valor_val=document.getElementById("valor_dado"+no).value;
+   // var emEstoque_val=document.getElementById("emEstoque_dado"+no).value;
+   // if (document.getElementById('simRadio').checked) {
+     //   emEstoque  = document.getElementById('simRadio').value;
+   //   }
+    //  else{
+    //    emEstoque = document.getElementById('naoRadio').value;
+    //  }
+  // var descricao_val=document.getElementById("descricao_dado"+no).value;
+   
+
+    document.getElementById("nome"+no).innerHTML=name_val;
+    document.getElementById("quantidade"+no).innerHTML=quantidade_val;
+    document.getElementById("validade"+no).innerHTML=validade_val;
+   // document.getElementById("tipoProduto"+no).innerHTML=tipoProduto_val;
+    document.getElementById("valor"+no).innerHTML=valor_val;
+   // document.getElementById("emestoque"+no).innerHTML=emEstoque_val;
+   // document.getElementById("textArea"+no).innerHTML=descricao_val;
+
+    document.getElementById("edit_button"+no).style.display="block";
+    document.getElementById("save_button"+no).style.display="none";
+}
+
+
+
+function delete_row(no){
+
+    document.getElementById("row"+no+"").outerHTML="";
+    
+}
 
 function adicionarDados(){
     
     contador++;
-    var tabela = document.getElementById('tabela');
-    var linha = tabela.insertRow(tabela.length);
-
+    
     var nome = document.getElementById('nome').value;
     var quantidade = document.getElementById('quantidade').value;
     var validade = document.getElementById('validade').value;
     var tipoProduto = document.getElementById('tipoProduto').value;
     var valor = document.getElementById('valor').value;
-    var emEstoque = document.getElementById('simRadio').value;
-    var emEstoqueNao = document.getElementById('naoRadio').value;
+    var emEstoque = document.getElementsByName('emEstoque').value;
+    if (document.getElementById('simRadio').checked) {
+        emEstoque  = document.getElementById('simRadio').value;
+      }
+      else{
+        emEstoque = document.getElementById('naoRadio').value;
+      }
     var descricao = document.getElementById('textArea').value;
+
+
+    var table = document.getElementById('tabela');
+    var table_len=(table.rows.length);
+    var row = table.insertRow(table_len).outerHTML="<tr id='row"+table_len+"'><td id='nome"+table_len+"'>"+nome+
+    "</td><td id='quantidade"+table_len+"'>"+quantidade+
+    "</td><td id='validade"+table_len+"'>"+validade+
+    "</td><td id='tipoProduto"+table_len+"'>"+tipoProduto+
+    "</td><td id='valor"+table_len+"'>"+valor+
+    "</td><td id='emEstoque"+table_len+"'>"+emEstoque+
+    "</td><td id='descricao"+table_len+"'>"+descricao+
+    "</td><td><input type='button' id='edit_button"+
+    table_len+"' value='Edit' class='edit' onclick='edit_row("+
+    table_len+")'> <input type='button' id='save_button"+
+    table_len+"' value='Save' class='save' onclick='save_row("+
+    table_len+")'> <input type='button' value='Delete' class='delete' onclick='delete_row("+table_len+")'></td></tr>";
     
-    var Selecao = linha.insertCell(0);
-    var cellNome = linha.insertCell(1);
-    var cellQuantidade = linha.insertCell(2);
-    var cellValidade = linha.insertCell(3);
-    var cellTipoProduto = linha.insertCell(4);
-    var cellValor = linha.insertCell(5);
-    var cellEmEstoque = linha.insertCell(6);
-    var celDescricao = linha.insertCell(7);
-    var btnEdiar = linha.insertCell(8);
-    var btnExcluir = linha.insertCell(8);
-    var btnSalvar = linha.insertCell(8);
-
-
-    Selecao.innerHTML = "<input type='checkbox'/>";
-    cellNome.innerHTML = nome;
-    cellQuantidade.innerHTML = quantidade;
-    cellValidade.innerHTML = validade;
-    cellTipoProduto.innerHTML = tipoProduto;
-    cellValor.innerHTML = valor;
-    cellEmEstoque.innerHTML = emEstoque;
-    celDescricao.innerHTML = descricao;
-    btnEdiar.innerHTML = "<button onclick= 'editarDados(this)' id='editarDados'>Editar</button>";
-    btnExcluir.innerHTML = "<button onclick= 'removeDados(this)' id='removeDados'>Excluir</button>";
-    btnSalvar.innerHTML = "<button onclick= 'salvarDados(this)'id='salvarDados'>Salvar</button>";
-
-    document.getElementById('editarDados').style.display="block";
-    document.getElementById('salvarDados').style.display="none";
-
-}
-
-function editarDados(linha){
-
-    document.getElementById('editarDados').style.display="none";
-    document.getElementById('salvarDados').style.display="block";
-
-    var tabela = document.getElementById('tabela');
-    var linha = tabela.insertRow(tabela.length);
-    var linhaSalvar = linha.parentNode.parentNode.rowIndex;
-
-    var nome = document.getElementById('nome').value;
-    var quantidade = document.getElementById('quantidade').value;
-    var validade = document.getElementById('validade').value;
-    var tipoProduto = document.getElementById('tipoProduto').value;
-    var valor = document.getElementById('valor').value;
-    var emEstoque = document.getElementById('simRadio').value;
-    var emEstoqueNao = document.getElementById('naoRadio').value;
-    var descricao = document.getElementById('textArea').value;
-    
-    var Selecao = linhaSalvar.insertCell(0);
-    var cellNome = linhaSalvar.insertCell(1);
-   // var cellQuantidade = linha.insertCell(2);
-   // var cellValidade = linha.insertCell(3);
-   // var cellTipoProduto = linha.insertCell(4);
-   // var cellValor = linha.insertCell(5);
-   // var cellEmEstoque = linha.insertCell(6);
-   // var celDescricao = linha.insertCell(7);
-   // var btnEdiar = linha.insertCell(8);
-   // var btnExcluir = linha.insertCell(8);
-   //var btnSalvar = linha.insertCell(8);
-
-    Selecao.innerHTML = "<input type='checkbox'/>";
-    cellNome.innerHTML = "<input type='text' id='nome' value='"+nome+"'>";
-    cellQuantidade.innerHTML = quantidade;
-    cellValidade.innerHTML = validade;
-    cellTipoProduto.innerHTML = tipoProduto;
-    cellValor.innerHTML = valor;
-    cellEmEstoque.innerHTML = emEstoque;
-    celDescricao.innerHTML = descricao;
-    btnEdiar.innerHTML = "<button onclick= 'editarDados(this)'>Editar</button>";
-    btnExcluir.innerHTML = "<button onclick= 'removeDados(this)'>Excluir</button>";
-    btnSalvar.innerHTML = "<button onclick= 'salvarDados(this)'>Salvar</button>";
-
-}
-
-
-function removeDados(linha){
-
-    var linhaExcluir = linha.parentNode.parentNode.rowIndex;
-    document.getElementById('tabela').deleteRow(linhaExcluir);
+    document.getElementById('nome').value="";
+    document.getElementById('quantidade').value="";
+    document.getElementById('validade').value="";
+    document.getElementById('tipoProduto').value="";
+    document.getElementById('valor').value="";
+    document.getElementsByName('emEstoque').value="";
+    document.getElementById('textArea').value="";
     
 }
